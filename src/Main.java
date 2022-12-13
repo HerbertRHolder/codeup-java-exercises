f089f0b79ec44ed92e12681f6fc34fb9a894a2d9
Herb <herbrholder@gmail.com>
2022-12-13 11:01:18 -0600

    added ControlFlowExercises class

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+//    main method shortcut
+//    main(tab/enter)
+// system.out.println
+//    sout
+
+
 }
+
+

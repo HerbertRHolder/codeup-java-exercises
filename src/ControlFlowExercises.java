2fc0f678e22d6ca0f2f3717625a99167bd674687
Herb <herbrholder@gmail.com>
2022-12-15 11:56:21 -0600

    Completed helloworld file

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,42 @@
+import java.util.*;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+//     int i = 5;
+     int n = 0;
+//     while ( i <= 15){
+//         System.out.println(i);
+//         i++;
+//     }
+//        do {
+//            System.out.println(n);
+//            n+=2;
+//        } while (n<=100);
+//        for (int i = 0;i<=100;i++){
+//
+//            if (i % 3 == 0) {
+//
+//                if (i % 3 == 0 & i % 5 == 0){
+//                    System.out.printf("FizzBuzz \n");
+//                }else {
+//                    System.out.printf("Fizz \n");
+//                }
+//
+//            }else if (i % 5== 0 ) {
+//
+//                if (i % 3 == 0 & i % 5 == 0){
+//                    System.out.printf("FizzBuzz \n");
+//                }else {
+//                    System.out.printf("Buzz \n");
+//                }
+//            }else {System.out.printf(String.valueOf(i)+ "\n");}
+//        } // end of for loop
 
-    }
-}
+
+
+
+
+    }// main end
+
+
+}// ControlFlowExercises end
---
+++ src/HelloWorld.java
@@ -0,0 +1,37 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int myFavoriteNumber = 2;
+        String myString = "This world is amazing";
+        myString = "a";
+//        myString = 3.14159;
+        long myNumber;
+//        myNumber = 3.14;
+        myNumber = 123L;
+        myNumber = (long) 3.14;
+        //        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int class = 0;
+//        int z = 5;
+//        System.out.println(++z);
+//        System.out.println(z);
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        if you increment a int past its constraints the java compiler will
+//        give an error because the datatype will not be compatible in memory
+
+
+
+    }
+}
